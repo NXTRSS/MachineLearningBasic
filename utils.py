4bf0f0104797fdb359db928d21c3bd6dbf305f3e
NXTRSS <kamillo_j@yahoo.com>
2022-04-16 12:35:14 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ import pickle
 import traitlets
 from ipywidgets import widgets
 from IPython.display import display
-# from tkinter import Tk, filedialog
 
 
 def decrypt_file(file_path, password):
@@ -38,42 +37,6 @@ def decrypt_pickle(file_path, password):
 
     return pickle.loads(fDec.getvalue())
 
-
-class SelectFilesButton(widgets.Button):
-    """A file widget that leverages tkinter.filedialog."""
-
-    def __init__(self):
-        super(SelectFilesButton, self).__init__()
-        # Add the selected_files trait
-        self.add_traits(files=traitlets.traitlets.List())
-        # Create the button.
-        self.description = "Select Files"
-        self.icon = "square-o"
-        self.style.button_color = "orange"
-        # Set on click behavior.
-        self.on_click(self.select_files)
-
-    @staticmethod
-    def select_files(b):
-        """Generate instance of tkinter.filedialog.
-
-        Parameters
-        ----------
-        b : obj:
-            An instance of ipywidgets.widgets.Button 
-        """
-        # Create Tk root
-        root = Tk()
-        # Hide the main window
-        root.withdraw()
-        # Raise the root to the top of all windows.
-        root.call('wm', 'attributes', '.', '-topmost', True)
-        # List of selected fileswill be set to b.value
-        b.files = filedialog.askopenfilename(multiple=True)
-
-        b.description = "Files Selected"
-        b.icon = "check-square-o"
-        b.style.button_color = "lightgreen"
         
 class bcolors:
     HEADER = '\033[95m'
